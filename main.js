fe5b21ee75f388a0da2c69b1da621960108d4029
Jack White <jfw793@gmail.com>
2021-08-16 14:11:29 +1000

    Bugfixes

--- main.js
+++ main.js
@@ -1,23 +1,38 @@
 const gameboard = (() => {
-    let array = ['','', '', '', '', '', '', '', ''];
-    const cells = document.querySelectorAll('.cell');
+    let array = [
+        '', '', '', 
+        '', '', '', 
+        '', '', '',
+    ];
+
+    const _cells = document.querySelectorAll('.cell');
+
+    const _winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
 
     const updateBoard = () => {
-        cells.forEach(function (cell, i) {
+        _cells.forEach(function (cell, i) {
             cell.innerText = gameboard.array[i]
         });
     }
 
     const gameLogic = () => {
-        //
+
     }
 
     const eventHandlers = () => {
-        cells.forEach(function (cell, i) {
-            cell.addEventListener('click', () => {
+        _cells.forEach(function (cell, i) {
+            cell.addEventListener('click', () => { // draw x's and o's on click
                 let arrayNullCount = gameboard.array.length - gameboard.array.filter(String).length;
-
-                if (arrayNullCount === 0) {
+                if (_cells[i].innerText !== '') {
                     return;
                 } else if (arrayNullCount % 2 == 0) {
                     gameboard.array.splice(i, 1, 'x');
